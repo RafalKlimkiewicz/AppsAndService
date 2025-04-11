1cc63b3766609196b4b97696d77de28f84988ca5
Rafal K. <klimek1008@gmail.com>
2025-04-11 23:59:01 +0200

    Times

---
+++ Chapter07/WorkingWithTime/Program.Helpers.cs
@@ -0,0 +1,30 @@
+﻿partial class Program
+{
+    private static void ConfigureConsole(string culture = "en-US", bool overrideComputerCulture = true)
+    {
+        OutputEncoding = System.Text.Encoding.UTF8;
+
+        Thread t = Thread.CurrentThread;
+
+        if (overrideComputerCulture)
+        {
+            t.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo(culture);
+            t.CurrentUICulture = t.CurrentCulture;
+        }
+
+        var ci = t.CurrentCulture;
+
+        WriteLine($"Current culture: {ci.DisplayName}");
+        WriteLine($"Short date pattern: {ci.DateTimeFormat.ShortDatePattern}");
+        WriteLine($"Long date pattern: {ci.DateTimeFormat.LongDatePattern}");
+        WriteLine();
+    }
+
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"*** {title}");
+        ForegroundColor = previousColor;
+    }
+}
---
+++ Chapter07/WorkingWithTime/Program.cs
@@ -0,0 +1,13 @@
+﻿ConfigureConsole("nl-NL");
+
+SectionTitle("Specifyng date and time values");
+
+WriteLine($"MinValue: {DateTime.MinValue}");
+WriteLine($"MaxValue: {DateTime.MaxValue}");
+WriteLine($"UnixEpoch: {DateTime.UnixEpoch}");
+WriteLine($"Now: {DateTime.Now}");
+WriteLine($"Today: {DateTime.Today}");
+WriteLine($"Today:d: {DateTime.Today:d}");
+WriteLine($"Today:D: {DateTime.Today:D}");
+WriteLine($"Today:o: {DateTime.Today:o}");
+WriteLine($"Today:R: {DateTime.Today:R}");
