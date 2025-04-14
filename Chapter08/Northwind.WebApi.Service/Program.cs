129ad35a317bcd12273be46f905cc0f156fbb16e
Rafal K. <klimek1008@gmail.com>
2025-04-14 23:32:47 +0200

    08|WebApi minimal api intro

---
+++ Chapter08/Northwind.WebApi.Service/Program.cs
@@ -0,0 +1,21 @@
+using DataContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddNorthwindContext();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
